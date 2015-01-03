8364fc2ac1e5dedc8d79795f5899347db44ad96c
BetterThan <fsl.abouri@gmail.com>
2015-01-03 19:30:08 +0100

    Ajout de la doc

--- metier/gestionbd.h
+++ metier/gestionbd.h
@@ -2,6 +2,9 @@
 #define GESTIONBD_H
 #include <QtSql>
 
+/**
+ * @brief The GestionBD classe permettant de gérer une base de donnée.
+ */
 class GestionBD
 {
 private:
@@ -9,11 +12,35 @@ QSqlDatabase db;
 QSqlQuery *query;
 bool connexionOk;
 public:
+/**
+     * @brief GestionBD Démarre une connexion avec la base de donnée GameBD.
+     */
     GestionBD();
+    /**
+      * @biref Destructeur de la classe GestionBD
+      * Déconnecte également de la base de donnée.
+      */
     ~GestionBD();
+    /**
+     * @brief requete Permet d'envoyer une requête à la base de donnée.
+     * @param requete La requête SQL.
+     * @return true si la requête est correcte, false sinon.
+     */
     bool requete(QString requete);
+    /**
+     * @brief estConnecte Indique si la connexion a bien eu lieu.
+     * @return true si la connexion s'est fait, false sinon.
+     */
     bool estConnecte() const;
+    /**
+     * @brief closeBD Permet de se déconnecter de la base de donnée.
+     */
     void closeBD();
+    /**
+     * @brief selectAll Envoie une requête qui permet de voir tout le contenu d'une table.
+     * @param table la table dont on veut le contenu.
+     * @return le contenu de la table.
+     */
     QString selectAll(QString table);
 };
 
